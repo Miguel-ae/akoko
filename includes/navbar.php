6f30224831ea93f915edd409b4e741391944540b
migueprz <migueprzm@gmail.com>
2020-05-11 14:15:57 +0200

    Pequeños retoques y contenido en pagina timer

---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+<!-- Libraries & start html,head,meta,links,... -->
+<?php include 'link.php';?>
+
+<body>
+
+    <!-- NAVBAR -->
+    <section class="h-100">
+      <nav class="navbar container wallpaper-navbar">
+        <div class="container">
+           <ul class="nav align-items-center">
+              <li><i class="far fa-clock"></i></li>
+              <li><a class="" href="#">Timer</a></li>
+              <li><a class="" href="#">Calendar</a></li>
+              <li><a class="" href="#">Tasks</a></li>
+              <li><a class="" href="#">Dashboard</a></li>
+            </ul>
+           <ul class="nav navbar-right align-items-center">
+              <li><a href="#">Profile</a></li>
+              <li><a class="aux-text" href="#">Log out</a></li>
+           </ul>          
+        </div>
+      </nav>
+    </section>
+    <!-- NAVBAR END-->
+
+</body>
+
+<!-- Scripts & final html -->
+<?php include 'includes/script.php' ?>
---
+++ timer.php
@@ -0,0 +1,55 @@
+<!-- Libraries & start html,head,meta,links,... -->
+<?php include 'includes/link.php';?>
+
+
+<body>
+
+    <!-- NAVBAR -->
+    <section class="h-100">
+      <nav class="navbar container wallpaper-navbar">
+        <div class="container">
+           <ul class="nav align-items-center">
+              <li><i class="far fa-clock"></i></li>
+              <li><a class="text-secondary" href="timer.php">Timer</a></li>
+              <li><a class="" href="#">Calendar</a></li>
+              <li><a class="" href="#">Tasks</a></li>
+              <li><a class="" href="#">Dashboard</a></li>
+            </ul>
+           <ul class="nav navbar-right align-items-center">
+              <li><a href="#">Profile</a></li>
+              <li><a class="aux-text" href="#">Log out</a></li>
+           </ul>          
+        </div>
+      </nav>
+    </section>
+    <!-- NAVBAR END-->
+
+
+    <!-- TIMER -->
+    <section class="">
+      
+      <div class="row p-5">
+        <div class="col-3">
+           <div class="field-border row align-items-center">
+             <i class="fas fa-calendar-alt"></i>
+             <div class="datetime-calendar">Lunes, 03 de Abril de 2020</div> <!-- Aqui cambiara la fecha a la fecha de hoy DATETIME CURRENT -->
+           </div>
+        </div>
+        <div class="col-9 pl-5">
+           <div class="field-border row align-items-center">
+             div
+           </div>
+        </div>
+      </div>
+
+    </section>
+    
+    <!-- TIMER END-->
+
+</body>
+
+
+
+
+<!-- Scripts & final html -->
+<?php include 'includes/script.php' ?>
